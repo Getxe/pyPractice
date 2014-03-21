e2c273e680c5ec855630fb251fe9f137ac461e71
Getxe <getxeone@gmail.com>
2014-03-21 16:21:20 +0400

    like it wokrs

--- bs.py
+++ bs.py
@@ -1,9 +1,6 @@
 ﻿from random import randint
 class Field(object):
     def __init__(self,size):
-        #self.board = [0] * size
-        #for i in range(size):
-        #    self.board[i] = [0] * size
         self.board = []
         for i in range(size):
             self.board.append([0] * size)
@@ -21,7 +18,7 @@ class Field(object):
         x = c['x']
         y = c['y']
         res = self.board[x][y]
-        self.board[1][y] = 2
+        self.board[x][y] = 2
         return res
 
     def display(self):
@@ -37,19 +34,20 @@ class Field(object):
 
     def is_end(self):
         for line in self.board:
-            if line.count(3)==1:
-                return True
+            if line.count(1)!=0:
+                return False
         else:
-            return False
+            return True
+
 
 
 def get_user_coord(size):
-    x = int(raw_input("Please, input column: "))
-    while x<0 or x>size-1:
-        x = int(raw_input("This value incorrect, please, try again: "))
-    y = int(raw_input("Please, input row: "))
+    y = int(raw_input("Please, input column: "))
     while y<0 or y>size-1:
-        y = int(raw_input("This value incorrect, please, try again: "))
+        y = int(raw_input("This value is incorrect, please, try again: "))
+    x = int(raw_input("Please, input row: "))
+    while x<0 or x>size-1:
+        x = int(raw_input("This value is incorrect, please, try again: "))
     coord = {'x':x,'y':y}
     return coord
 
@@ -65,11 +63,33 @@ def get_coord(size,plr):
     else:
         return {'x':-1, 'y':-1 }
 
-"""fl = Field(4)
-print fl
-print fl.is_end()
-fl.board[3][3]=3
-print fl
-print fl.is_end()"""
+
+num_of_ships = 1
+size = 5
+fl = Field(size)
+#random initialization ships
+for i in range(0, num_of_ships):
+    coord = get_coord(size,'r')
+    x = coord['x']
+    y = coord['y']
+    fl.board[x][y] = 1
+i=1
+print "Step %s" % i
+fl.display()
+while(not fl.is_end()):
+    c = get_coord(size,'i')
+    res = fl.modif(c)
+    if res == 2:
+        print "We have shooted this coordinates already! Try other coordintes!"
+        continue
+    elif res == 1:
+        print "You hit!"
+    else:
+        print "You missed."
+    i+=1
+    print "Step %s" % i
+    fl.display()
+
+print "WIN!"
 
 
