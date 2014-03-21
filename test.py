a939963c9b167d495cd69294239317892db97692
Getxe <getxeone@gmail.com>
2014-03-21 19:24:52 +0400

    уже похоже на правду

--- test.py
+++ test.py
@@ -4,44 +4,64 @@ from bs import *
 root = Tk()
 num_of_ships = 1
 size = 5
-fl = Field(size)
+
+def Add_btns(size):
+	global btns
+	btns = []
+	for i in xrange(0,size):
+		btns.append([])
+		for j in xrange(0,size):
+			btn = Button(root, width = 4, height= 2, text = ' ')
+			btn.grid(row = i, column = j)
+			btn.config(state = 'disabled', relief=SUNKEN, borderwidth=1)
+			btns[i].append(btn)
+	return btns
+
+def btns_disable(btns):
+        for i in range(0,len(btns)):
+           for j in range(0,len(btns[i])):
+                btns[i][j].config(state = 'disabled', relief=SUNKEN, borderwidth=1)
 
 def newGame():
+    global fl 
+    fl = Field(size)
     print "Choose pistions of shops"
     for i in btns:
     	for b in i:
-    		b.config(state = 'active', relief=RAISED, bg='white',text = 'O')
+    		b.config(state = 'active', relief=RAISED,text = 'O')
     		b.bind("<Button-1>", get_coord_g)
-
-
-	fl = Field(size)
 	#random initialization ships
-	for i in range(0, num_of_ships):
-	    coord = get_coord(size,'r')
-	    x = coord['x']
-	    y = coord['y']
-	    fl.board[x][y] = 1
-	print "GOAL :",x,y
-	print "FIELD"
-	print fl
-
+    for i in range(0, num_of_ships):
+    	print i
+        coord = get_coord(size,'r')
+        x = coord['x']
+        y = coord['y']
+        fl.board[x][y] = 1
 
 def  get_coord_g(event):
-
+    global fl, btns
     grid_info = event.widget.grid_info()
-    x = int(grid_info["row"])
-    y = int(grid_info["column"])
-    c = {'x':y, 'y':x}
-    print "coordinates of click",x,y
+    y = int(grid_info["row"])
+    x = int(grid_info["column"])
+    c = {'x':x, 'y':y}
+    #print "coordinates of click",x,y
     res = fl.modif(c)
-    print res
-    if res == 2:
-        print "We have shooted this coordinates already! Try other coordintes!"
-    elif res == 1:
-        print "You hit!"
-    else:
-        print "You missed."
-	print fl
+    btns[y][x].config(state = 'disabled', relief=SUNKEN)
+    if res == 1:
+    	btns[y][x].config(text="X")
+        #print "We have shooted this coordinates already! Try other coordintes!"
+    #elif res == 1:
+        #for i in range(0,len(btns)):
+        #   for j in range(0,len(btns[i])):
+        #       if i == y and j == x :     	        
+        #print "You hit!"
+    #else:
+      
+        #print "You missed."
+    if fl.is_end():
+    	print "Game over"
+    	btns_disable(btns)
+	
 
 
 # create a toplevel menu
@@ -50,17 +70,16 @@ menubar.add_command(label="Hello!", command=newGame)
 menubar.add_command(label="Quit!", command=root.quit)
 
 box = Frame(root)
-btns = []
-for i in xrange(0,size):
+btns = Add_btns(size)
+"""for i in xrange(0,size):
 	btns.append([])
 	for j in xrange(0,size):
-		btn = Button(root,width = 2, text = ' ')
+		btn = Button(root, width = 4, height= 2, text = ' ')
 		btn.grid(row = i, column = j)
-		btn.config(state = 'disabled',bg='white', relief=SUNKEN, borderwidth=1)
-		btns[i].append(btn)
-
-
+		btn.config(state = 'disabled', relief=SUNKEN, borderwidth=1)
+		btns[i].append(btn)"""
 
 # display the menu
 root.config(menu=menubar)
 root.mainloop()
+
